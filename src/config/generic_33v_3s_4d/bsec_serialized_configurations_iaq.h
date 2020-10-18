8c20b87298655cfea26f75a6f749170ffb3810dd
Julio Fontes <juliofontes@ua.pt>
2020-10-18 19:29:28 +0100

    Added Support for BSEC 1.4.8.0_

---
+++ src/config/generic_33v_3s_4d/bsec_serialized_configurations_iaq.h
@@ -0,0 +1,16 @@
+#ifndef BSEC_CONFIGS
+#define BSEC_CONFIGS
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const uint8_t bsec_config_iaq[454];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
